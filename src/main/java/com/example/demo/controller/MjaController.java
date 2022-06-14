14f423984a492a0eb984dab9d5c6b54e93fb543c
조경은 <universe2261877@gmail.com>
2022-06-14 15:22:17 +0900

    login jsp 생성, utils, popup 등

--- src/main/java/com/example/demo/controller/MjaController.java
+++ src/main/java/com/example/demo/controller/MjaController.java
@@ -2,13 +2,22 @@ package com.example.demo.controller;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.common.CommonProperties;
 import com.example.demo.service.IMjaService;
+import com.example.demo.util.Utils;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class MjaController {
@@ -43,6 +52,36 @@ public class MjaController {
 	public String login() {
 		return "login";
 	}
+	
+	@RequestMapping(value = "loginAjax", method = RequestMethod.POST, produces = "text/json;charset=UTF-8")
+	@ResponseBody
+	public String loginAjax(@RequestParam HashMap<String, String> params, HttpSession session) throws Throwable {
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		
+		try {
+			//패스워드 암호화
+			params.put("pw", Utils.encryptAES128(params.get("pw")));
+
+			HashMap<String, String> data = iMjaService.getLogin(params);
+			
+			if (data != null && !data.isEmpty()) {
+				session.setAttribute("nName", data.get("NICK_NAME"));
+
+				modelMap.put("res", CommonProperties.RESULT_SUCCESS);
+			} else {
+				modelMap.put("res", CommonProperties.RESULT_FAILED);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			modelMap.put("res", CommonProperties.RESULT_ERROR);
+		}
+		
+		
+		return mapper.writeValueAsString(modelMap);
+	}
+	
 	
 	@GetMapping ("/signUp") 
 	public String signUp() {
