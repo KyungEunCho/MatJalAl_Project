9acbd26c2ca05f3537e6c9175510e387e75702c9
Gwakdahun <ekgns7459@daum.net>
2022-06-29 19:33:13 +0900

    file upload

--- src/main/java/com/example/demo/MatJalAlApplication.java
+++ src/main/java/com/example/demo/MatJalAlApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.demo.FileUpload.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FileStorageProperties.class)
 public class MatJalAlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MatJalAlApplication.class, args);
-	}
+   public static void main(String[] args) {
+      SpringApplication.run(MatJalAlApplication.class, args);
+   }
 
 }
