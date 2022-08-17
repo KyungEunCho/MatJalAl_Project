8f85ff82a2ceec98253bd30ba3133512a7c716bb
Gwakdahun <ekgns7459@daum.net>
2022-08-17 14:07:14 +0900

    다시 푸쉬

---
+++ src/main/java/com/example/demo/dto/KakaoDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+
+public class KakaoDTO {
+
+	private long k_number;
+	private String k_name;
+	private String k_email;
+
+	}
---
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -0,0 +1,29 @@
+package com.example.demo.repository;
+
+import java.util.HashMap;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.dto.KakaoDTO;
+
+@Repository
+public class MemberRepository {
+	
+	// mapper를 호출하기 위한 클래스 주입
+	@Autowired
+	private SqlSessionTemplate sql;
+
+	public void kakaoInsert(HashMap<String, Object> userInfo) {
+		sql.insert("Member.kakaoInsert",userInfo);
+	}
+	
+	public KakaoDTO findkakao(HashMap<String, Object> userInfo) {
+		System.out.println("RN:"+userInfo.get("nickname"));
+		System.out.println("RE:"+userInfo.get("email"));
+	return sql.selectOne("Member.findKakao", userInfo);
+	}
+
+
+}
